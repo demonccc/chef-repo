451a31e2f138edc324737c9bfd34d017985abb7f
Claudio Cesar Sanchez Tejeda <sanchez@nimbuzz.com>
2012-10-19 11:40:39 -0300

    Modified heartbeat3 cookbook.

--- cookbooks/heartbeat3/metadata.rb
+++ cookbooks/heartbeat3/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "demonccc@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures heartbeat"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.4.2"
+version           "0.4.3"
 
 recipe "heartbeat3", "Installs and configures heartbeat"
 recipe "heartbeat3::services", "Sanitizes the chef service commands of the services owned by Heartbeat"
